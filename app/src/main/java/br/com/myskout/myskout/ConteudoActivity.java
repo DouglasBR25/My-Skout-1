dcdc0c5c00ecc15d564e15f7dfe57565fc9c4d1d
DouglasBR25 <douglas2502@gmail.com>
2022-08-10 10:56:43 -0300

    first commit

---
+++ app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
@@ -0,0 +1,25 @@
+package br.com.myskout.myskout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ConteudoActivity extends AppCompatActivity {
+    //Declarando a variável global para o componente
+    BottomNavigationView bottomNavigationView;
+
+    //Instanciar todos os fragments que serão utilizados neste layout
+    TalentosFragment talentosFragment = new TalentosFragment();
+    VagasFragment vagasFragment = new VagasFragment();
+    RecrutadoresFragment recrutadoresFragment = new RecrutadoresFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conteudo_layout);
+
+        bottomNavigationView = findViewById(R.id.idBottomNavView);
+    }
+}
